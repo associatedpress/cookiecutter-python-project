331b189ce58a890cafcefb14f9f47ba64d316bd2
Nicky Forster <nforster@ap.org>
2019-09-09 18:41:41 -0500

    removed matplotlib pipenv install from hook, added prompt-toolkit install with specified version

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 command -v pipenv >/dev/null 2>&1 || { echo >&2 "pipenv not found.  Aborting startup script."; exit 1; }
 echo "PYTHONSTARTUP=$PWD/.startup.py" >> .env
-pipenv install
+pipenv --three
+## ipykernel requires the latest prompt-toolkit library
+pipenv install "prompt-toolkit>=2.0.4"
 pipenv install ipykernel
 pipenv install pandas
-pipenv install matplotlib
 pipenv run python -m ipykernel install --user --name={{ cookiecutter.project_slug }}
