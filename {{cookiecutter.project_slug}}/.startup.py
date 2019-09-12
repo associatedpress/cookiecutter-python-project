8b773514482bf87db4c1f8f14df8274f331f4df0
Nicky Forster <nforster@ap.org>
2019-09-12 10:30:46 -0500

    removed comments from .startup.py

--- {{cookiecutter.project_slug}}/.startup.py
+++ {{cookiecutter.project_slug}}/.startup.py
@@ -1,19 +1,9 @@
 import os
-## This is the project directory name to set as root
 project_name = f"{{ cookiecutter.project_slug }}"
-## This is the current directory
 cwd = os.getcwd()
-## This is the basename of the current directory path
 basename = os.path.basename(cwd)
-## If we get all the way to the root of the file system,
-## We'll use this to not change the directory
 project_root_not_found = False
 
-## While the basename does not equal the project name
-## Use the split method to get the tail and head of the cwd
-## The new cwd becomes the tail and we test it again
-## If the head is ever an empty string, we found nothing, and
-## do not change the directory.
 while basename != project_name:
     if os.path.split(cwd)[1] == '':
         basename = project_name
