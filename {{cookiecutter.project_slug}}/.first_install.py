5c0f3e437c85a6f82cbc2128c21163109f9505e0
Nicky Forster <nforster@ap.org>
2020-10-05 10:02:29 -0400

    custom pipenv script handles user-based environment customization now

---
+++ {{cookiecutter.project_slug}}/.first_install.py
@@ -0,0 +1,10 @@
+import os
+
+with open ('.env', 'w') as env_fi:
+    env_fi.write(f"PYTHONSTARTUP={os.getcwd()}/.startup.py\n") 
+
+os.system('pipenv install --dev')
+os.system('python -m ipykernel install --user --name={{ cookiecutter.project_slug }}')
+os.system('jupyter lab build')
+#  Generate ipynb for every markdown file in analysis
+os.system('jupytext --set-formats md,ipynb analysis/*.md')
