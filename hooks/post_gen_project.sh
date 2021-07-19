2bb2e19c5833c0c087ba5620081c96b7a2a83179
Nicky Forster <nforster@ap.org>
2021-07-19 16:51:04 -0400

    multiple installs now being handled by one install. notes added, nbconvert script line added to Pipfile

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
+## This post project generation script only runs if pipenv is on the machine
 command -v pipenv >/dev/null 2>&1 || { echo >&2 "pipenv not found.  Aborting startup script."; exit 1; }
-## ipykernel requires the latest prompt-toolkit library
-pipenv install "prompt-toolkit>=2.0.4"
-pipenv install "importlib-metadata<2,>=0.12"
-pipenv install ipykernel
-pipenv install pandas
-pipenv install matplotlib
-pipenv install notebook
-pipenv install jupyterlab
-pipenv install jupytext
-
-## This script will be run when someone clones into the project, it takes care
-## of what this hook puts in place two lines later.
+## Main installs typical to a project
+pipenv install ipykernel pandas matplotlib notebook jupyterlab jupytext
+## Add the first install script to the Pipfile
 echo -e "\n[scripts]\nfirst_install = \"python .first_install.py\"" >> Pipfile
-## These are the "two lines later"
-echo "PYTHONSTARTUP=$PWD/.startup.py" >> .env
-pipenv run python -m ipykernel install --user --name={{ cookiecutter.project_slug }}
+## Add script to render reports using AP nbconvert template
+echo -e "\nexport_html_notebook = \"jupyter-nbconvert --to html --template ap_report --execute --output-dir ./data/html_reports/ --TemplateExporter.extra_template_basedirs=./.nbconvert_templates\"" >> Pipfile
+## Run first_install script
+## This is meant to be run when people first clone the project.
+## We run it here to get the benefit of installing the ipython kernel.
+pipenv run first_install
