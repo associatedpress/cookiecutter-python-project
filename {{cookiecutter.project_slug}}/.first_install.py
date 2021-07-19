8cfbc6daedc9a5969a424d7900d7422adb605e98
Nicky Forster <nforster@ap.org>
2021-07-19 16:49:40 -0400

    added notes

--- {{cookiecutter.project_slug}}/.first_install.py
+++ {{cookiecutter.project_slug}}/.first_install.py
@@ -4,7 +4,8 @@ with open ('.env', 'w') as env_fi:
     env_fi.write(f"PYTHONSTARTUP={os.getcwd()}/.startup.py\n") 
 
 os.system('pipenv install --dev')
+# Install the ipython kernel with the project name
 os.system('python -m ipykernel install --user --name={{ cookiecutter.project_slug }}')
 os.system('jupyter lab build')
-#  Generate ipynb for every markdown file in analysis
+# Generate ipynb for every markdown file in analysis
 os.system('jupytext --set-formats md,ipynb analysis/*.md')
