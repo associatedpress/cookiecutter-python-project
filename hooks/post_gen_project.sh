e6fb0ca7eee66ef04417476ddac812a9a2670e45
Nicky Forster <nforster@ap.org>
2021-09-17 01:09:55 -0400

    jupyterlab templates and altair added to initial pipenv install

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,7 +2,7 @@
 ## This post project generation script only runs if pipenv is on the machine
 command -v pipenv >/dev/null 2>&1 || { echo >&2 "pipenv not found.  Aborting startup script."; exit 1; }
 ## Main installs typical to a project
-pipenv install ipython ipykernel pandas matplotlib notebook jupyterlab jupytext
+pipenv install ipython ipykernel pandas matplotlib notebook jupyterlab altair jupytext jupyterlab_templates
 ## Add the first install script to the Pipfile
 echo -e "\n[scripts]\nfirst_install = \"python .first_install.py\"\nexport_rmarkdown = \"Rscript .export_rmarkdown.R\"" >> Pipfile
 ## Add script to render reports using AP nbconvert template
