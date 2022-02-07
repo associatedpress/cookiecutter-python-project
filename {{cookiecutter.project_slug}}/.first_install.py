7b35a6d3f7a9f86c278efb84042da360149490f7
lfenn <lfenn@ap.org>
2022-02-07 18:34:33 -0500

    Add in the kernel tweak for cloned projects
    The kernel tweak from commit 181807 only made sure the kernels were properly set
    up on the original machine; anyone cloning the project would not have the
    tweaked kernels, and would thus still have issues running the jupyter notebooks
    correctly. This makes it so `.first_install.py` also adds the tweaked kernels to
    the local machine when it's run after cloning.

--- {{cookiecutter.project_slug}}/.first_install.py
+++ {{cookiecutter.project_slug}}/.first_install.py
@@ -1,18 +1,20 @@
 import os
 from subprocess import check_output
+from subprocess import run
 
 with open ('.env', 'w') as env_fi:
-    env_fi.write(f"PYTHONSTARTUP={os.getcwd()}/.startup.py\n") 
+    env_fi.write(f"PYTHONSTARTUP={os.getcwd()}/.startup.py\n")
 # Need to tell R which python executable to use
 with open ('.Renviron', 'w') as Renv_fi:
     Renv_fi.write(f"RETICULATE_PYTHON={check_output(['pipenv', 'run', 'which', 'python']).decode('utf-8')}")
 
-os.system('pipenv install --dev')
+run(['pipenv', 'install', '--dev'])
 # Install the ipython kernel with the project name
-os.system('python -m ipykernel install --user --name={{ cookiecutter.project_slug }}')
-os.system('jupyter lab build')
+run(['python', '-m', 'ipykernel', 'install', '--user', '--name={{ cookiecutter.project_slug }}'])
+run(['jupyter', 'lab', 'build'])
 # Generate ipynb for every markdown file in analysis
-os.system('jupytext --set-formats md,ipynb analysis/*.md')
+run(['jupytext', '--set-formats', 'md,ipynb', 'analysis/*.md'])
 # Enable template server
-os.system('jupyter labextension install jupyterlab_templates')
-os.system('jupyter serverextension enable --py jupyterlab_templates')
+run(['jupyter', 'labextension', 'install', 'jupyterlab_templates'])
+run(['jupyter', 'serverextension', 'enable', '--py', 'jupyterlab_templates'])
+run(['bash', '.set_kernel_path.sh'])
---
+++ {{cookiecutter.project_slug}}/.set_kernel_path.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Set kernels to run from where git was initialized (the project root)
+## If git wasn't initialized, notebooks run from where the notebook file is (the default)
+
+JUPYTER_DATA_DIR=$(jupyter --data-dir)
+KERNEL_PATH=$JUPYTER_DATA_DIR/kernels/{{cookiecutter.project_slug}}
+VENV_DIR=$(pipenv --venv)
+
+## Replace the default kernel.json with one that points to kernel.sh
+echo -e '{\n "argv": [' > $KERNEL_PATH/kernel.json
+echo "  \"$KERNEL_PATH/kernel.sh\"," >> $KERNEL_PATH/kernel.json
+echo -e '  "{connection_file}"\n ],\n "display_name": "{{cookiecutter.project_slug}}",\n "language":"python",\n "metadata":{\n "debugger":true\n }\n}' >> $KERNEL_PATH/kernel.json
+
+## kernel.sh first changes directory to git root before invoking the default command to launch a kernel
+echo -e '#!/bin/bash\ncd "$(git rev-parse --show-toplevel)"' > $KERNEL_PATH/kernel.sh
+echo "exec $VENV_DIR/bin/python -m ipykernel_launcher -f \"\$1\"" >> $KERNEL_PATH/kernel.sh
+
+## Execute permissions
+chmod 777 $KERNEL_PATH/kernel.sh
