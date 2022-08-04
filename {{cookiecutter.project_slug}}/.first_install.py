6cd7ebf47e2a1cebf50566a5a3482775cecb1bbd
Nicky Forster <nforster@ap.org>
2022-08-04 18:18:36 -0400

    script switches between a project create install and a later install based on whether Pipfile exists, explicit pyenv python location used for pipenv install, script expects to be run without pipenv run now

--- {{cookiecutter.project_slug}}/.first_install.py
+++ {{cookiecutter.project_slug}}/.first_install.py
@@ -1,13 +1,24 @@
-import os
+import os.path
 import glob
 from subprocess import check_output
 from subprocess import run
 
+run(['mkdir', './.venv'])
+
+PYENV_VERSION = "".join(check_output(['pyenv', 'version-name']).decode('utf-8').split())
+PYENV_PREFIX = "".join(check_output(['pyenv', 'prefix', f"{PYENV_VERSION}"]).decode('utf-8').split())
+
+if os.path.isfile('./Pipfile'):
+    run(['pipenv', 'install', '--dev'])
+else:
+    run(['pipenv', 'install', '--python', f"{PYENV_PREFIX}/bin/python", 'ipython', 'ipykernel', 'pandas', 'matplotlib', 'notebook', 'jupyterlab', 'altair', 'jupytext', 'jupyterlab_templates'])
+    ## Add this script to the Pipfile, along with the rmarkdown export script
+    with open('Pipfile', 'a') as pipfile:
+        pipfile.write('\n[scripts]\nexport_rmarkdown = "Rscript .export_rmarkdown.R"')
+
 VENV_DIR = "".join(check_output(['pipenv', '--venv']).decode('utf-8').split())
 RETICULATE_PYTHON = check_output(['pipenv', 'run', 'which', 'python']).decode('utf-8')
 TEMPLATE_PATHS = glob.glob('analysis/notebook_templates/*')
-# Install the necessary packages
-run(['pipenv', 'install', '--dev'])
 
 # Need to set the Jupyter data directory, this is where jupyter looks for kernels
 with open ('.env', 'w') as env_fi:
@@ -16,13 +27,11 @@ with open ('.env', 'w') as env_fi:
 with open ('.Renviron', 'w') as Renv_fi:
     Renv_fi.write(f"RETICULATE_PYTHON={RETICULATE_PYTHON}")
 
-# Need to build jupyter lab after installing jupyter plugins (like jupytext)
-run(['jupyter', 'lab', 'build'])
 # Generate ipynb for every markdown file in analysis
-run(['jupytext', '--set-formats', 'Rmd,ipynb', 'analysis/*.md'])
+run(['pipenv', 'run', 'jupytext', '--set-formats', 'Rmd,ipynb', 'analysis/*.md'])
 # Install jupyter template extension and enable the template server
-run(['jupyter', 'labextension', 'install', 'jupyterlab_templates'])
-run(['jupyter', 'serverextension', 'enable', '--py', 'jupyterlab_templates'])
+run(['pipenv', 'run', 'jupyter', 'labextension', 'install', 'jupyterlab_templates'])
+run(['pipenv', 'run', 'jupyter', 'serverextension', 'enable', '--py', 'jupyterlab_templates'])
 run(['mkdir', f"{VENV_DIR}/share/jupyter/notebook_templates"])
 for path in TEMPLATE_PATHS:
     run(['cp', '-r', path, f"{VENV_DIR}/share/jupyter/notebook_templates/"])
