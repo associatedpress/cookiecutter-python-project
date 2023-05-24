6d4b379dba80ac5025d3195039a39a0987df7eb6
Chris Keller <christopherlawrencekeller@gmail.com>
2023-05-24 12:56:35 -0600

    adds line to send {{cookiecutter.additional_packages_to_install}} as a sys.argv to the .first_install.py script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,4 +7,5 @@ command -v pipenv >/dev/null 2>&1 || { echo >&2 "pipenv not found.  Aborting sta
 #### Running it here to add jupyter data directory env variable, to set the RETICULATE_PYTHON r env
 ###### variable, to set up the jupyter lab template directory/enable its server,
 ###### and to set up the git solution for changing cwd in an analysis file.
-python ./.first_install.py
+
+python ./.first_install.py "{{cookiecutter.additional_packages_to_install}}"
